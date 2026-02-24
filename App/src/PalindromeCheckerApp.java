b047df73f14ddd381202c1092209a3806b2c8376
Hridey <hc6721@srmist.edu.in>
2026-02-24 14:40:24 +0530

    Snehapriya Added UC2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
 
-public class PalindromeCheckerApp {
-}
+
+public class PalindromeChecker {
+
+        public static void main(String[] args) {
+
+            // Hardcoded string
+            String text = "madam";
+
+            // Check if palindrome
+            boolean isPalindrome = true;
+
+            int left = 0;
+            int right = text.length() - 1;
+
+            while (left < right) {
+                if (text.charAt(left) != text.charAt(right)) {
+                    isPalindrome = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+
+            // Print result
+            if (isPalindrome) {
+                System.out.println("\"" + text + "\" is a Palindrome.");
+            } else {
+                System.out.println("\"" + text + "\" is NOT a Palindrome.");
+            }
+        }
+    }
